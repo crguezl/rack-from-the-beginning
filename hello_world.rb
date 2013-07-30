59ecb59ba34d9fffbcbd690c0409f1544cb59850
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-07-30 12:40:01 +0100

    The point is to illustrate that you can manipulate env (or response)

--- hello_world.rb
+++ hello_world.rb
@@ -2,60 +2,30 @@
 require 'rack'
 require 'rack/server'
 
-class HelloWorldApp
+class ParamsParser
   def initialize(app)
     @app = app
   end
 
-#  def self.call(env)
-#    # Rack::Request is simply a proxy for the env hash. 
-#    # The underlying env hash is modified 
-#    request = Rack::Request.new env
-#
-#    puts "DEFAULT_PORTS = #{Rack::Request::DEFAULT_PORTS}"
-#    puts "request.get? = #{request.get?}"
-#    # contains the union of GET and POST params
-#    puts "request.params = #{request.params.inspect}"
-#    # requested with AJAX
-#    puts "request.xhr? = #{request.xhr?.inspect}"
-#    # the incoming request IO stream
-#    puts "request.body = #{request.body.inspect}" 
-#    # cookies
-#    puts "request.cookies = #{request.cookies.inspect}" 
-#    # path 
-#    puts "request.fullpath = #{request.fullpath.inspect}" 
-#    # url 
-#    puts "request.url = #{request.url.inspect}" 
-#    # ip
-#    puts "request.ip = #{request.ip.inspect}" 
-#    # port
-#    puts "request.port = #{request.port.inspect}" 
-#    puts "GET() = #{request.GET()}"
-#
-#    if request.params['message']
-#      [200, {}, request.params['message']]
-#    else
-#      [200, {}, 'Say something to me!']
-#    end
-#  end
-
-  # Rack::Response is an abstraction around 
-  # generating response triplets. It simplifies access to 
-  # headers, cookies, and the body
-  def self.call(env)
-    response = Rack::Response.new
-    response.write 'Hello World' # write some content to the body
-    #response.body = ['Hello World'] # or set it directly
-    response['X-Custom-Header'] = 'foo'
-    response.set_cookie 'bar', 'baz'
-    response.status = 202
-    response.write("\nHola Mundo!\n")
-
-    puts response.inspect
-    response.finish # return the generated triplet
+  def call(env)
+    request = Rack::Request.new env
+    env['chuchu'] = request.params
+    #app.call env
+    env
   end
-
 end
 
+class HelloWorldApp
+  def self.call(env)
+    parser = ParamsParser.new self
+    env = parser.call env
+    # env['chuchu'] is now set to a hash for all the input paramters
+    for k in env.keys do
+      puts "#{k} => #{env[k]}" 
+    end
+
+    [200, {}, env['chuchu']] 
+  end
+end
 
 Rack::Server.start :app => HelloWorldApp, :server => 'thin'
